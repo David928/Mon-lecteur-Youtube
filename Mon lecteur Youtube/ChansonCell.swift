e5c67020295d0c01f31ad3eb9799653bd6f435f0
David928 <david.gonzales928@gmail.com>
2018-08-24 00:30:48 +0200

    chansons cell et mise en place

---
+++ Mon lecteur Youtube/ChansonCell.swift
@@ -0,0 +1,38 @@
+//
+//  ChansonCell.swift
+//  Mon lecteur Youtube
+//
+//  Created by GONZALES David on 24/08/2018.
+//  Copyright © 2018 GONZALES David. All rights reserved.
+//
+
+import UIKit
+
+class ChansonCell: UITableViewCell {
+    
+    @IBOutlet weak var miniature: UIImageView!
+    @IBOutlet weak var artisteEtTitreLabel: UILabel!
+    
+    var chanson: Chanson!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+    func creerCell(_ chanson: Chanson) {
+        self.chanson = chanson
+        
+        let attributed = NSMutableAttributedString(string: self.chanson.titre, attributes: [.font: UIFont.boldSystemFont(ofSize: 20), .foregroundColor: UIColor.black])
+        let secondeLigne = NSAttributedString(string: "\n" + self.chanson.artiste, attributes: [.font: UIFont.italicSystemFont(ofSize: 20), .foregroundColor: UIColor.black])
+        attributed.append(secondeLigne)
+        artisteEtTitreLabel.attributedText = attributed
+        
+    }
+    
+}
--- Mon lecteur Youtube/TableauController.swift
+++ Mon lecteur Youtube/TableauController.swift
@@ -13,6 +13,8 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
     @IBOutlet weak var tableView: UITableView!
     
     var chansons = [Chanson]()
+    
+    let identifiantCell = "ChansonCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +29,17 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let chanson = chansons[indexPath.row]
+        if let cell = tableView.dequeueReusableCell(withIdentifier: identifiantCell) as? ChansonCell {
+            cell.creerCell(chanson)
+            return cell
+        }
+        
         return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 170
     }
     
     func ajouterChanson() {
